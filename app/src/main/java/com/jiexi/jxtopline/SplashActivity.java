398e872251e7a52d59a27b5e31b860bc0c802488
Gou <13408696398@163.com>
2020-04-01 14:35:34 +0800

    first

---
+++ app/src/main/java/com/jiexi/jxtopline/SplashActivity.java
@@ -0,0 +1,62 @@
+package com.jiexi.jxtopline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView adbtn;
+    Button skipbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        initView();
+        init();
+    }
+    private void init() {
+        //利用Timer让此界面延迟3秒后再跳转,timer中有一个线程,这个线程不断执行task
+        Timer timer = new Timer();
+        //timertask实现runnable接口,TimerTask类表示一个在指定时间内执行的task
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                skip();
+            }
+        };
+        timer.schedule(task, 3000); //设置这个task在延迟三秒之后自动执行
+    }
+    private void initView(){
+        adbtn=(ImageView) findViewById(R.id.ad_img) ;
+        skipbtn=(Button) findViewById(R.id.skip_btn);
+        //跳过按钮点击事件，点击跳转到主界面
+        skipbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                skip();
+            }
+        });
+        //广告事件
+        adbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("http://www.baidu.com");    //设置跳转的网站
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+    }
+    private void skip(){
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        SplashActivity.this.finish();
+    }
+}
